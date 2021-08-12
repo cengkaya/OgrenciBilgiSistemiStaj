c49735db9957f03b59ee7e39823392d9fcda7e7f
kayalogg <kayayusuf18@gmail.com>
2021-08-12 19:32:17 +0300

    dbedit

---
+++ OgrenciBilgiSistemi/DbControl/DbController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace OgrenciBilgiSistemi
+{
+    class DbController
+    {
+        public static MySqlConnection conn = new MySqlConnection(@"server=localhost;user id=root;password=admin;database=ogrencibilgisistem");
+
+        public static void Connect()
+        {
+            if (conn.State != ConnectionState.Open)
+            {
+                try
+                {
+                    conn.Open();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+
+                }
+                
+
+            }
+
+        }
+        public static void Disconnect()
+        {
+            if (conn.State != ConnectionState.Closed)
+            {
+
+                try
+                {
+                    conn.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            
+
+        }
+
+    }
+}
+
+
